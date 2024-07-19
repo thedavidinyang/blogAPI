8b1dfc7f5769fb4ec68e319615059ce0bb1fd4ed
David Inyang <davidinyang01@gmail.com>
2024-07-19 11:17:04 +0100

    likes migration

--- database/migrations/2024_07_19_101329_create_likes_table.php
+++ database/migrations/2024_07_19_101329_create_likes_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+
         });
     }
 
