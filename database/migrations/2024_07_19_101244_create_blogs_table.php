83e5c81d7e1e6e19e163f3f4e094b551e5d290c2
David Inyang <davidinyang01@gmail.com>
2024-07-19 11:15:50 +0100

    blog migration

--- database/migrations/2024_07_19_101244_create_blogs_table.php
+++ database/migrations/2024_07_19_101244_create_blogs_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->text('description')->nullable();
         });
     }
 
