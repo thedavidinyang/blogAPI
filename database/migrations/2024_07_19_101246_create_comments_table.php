f999323f129c48b8052c8497a1326c43034dac5f
David Inyang <davidinyang01@gmail.com>
2024-07-19 11:17:27 +0100

    comments migration

--- database/migrations/2024_07_19_101246_create_comments_table.php
+++ database/migrations/2024_07_19_101246_create_comments_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->text('content');
         });
     }
 
