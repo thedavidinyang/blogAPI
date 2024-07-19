f77299272adbde1d8b854470d22192677c99b13d
David Inyang <davidinyang01@gmail.com>
2024-07-19 14:03:40 +0100

    readme update

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\LikeController;
-use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,27 +16,22 @@ use App\Http\Controllers\CommentController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
-
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::middleware('token')->group(function () {
 
-    Route::put('test', function(){
+    Route::put('test', function () {
 
         return '1234';
     });
-    // Route::post('blog', [BlogController::class, 'store']);
-
 
     Route::apiResource('blogs', BlogController::class);
 
     Route::apiResource('blogs.posts', PostController::class)->shallow();
-    // Route::put('/blogs/{blog}', [BlogController::class, 'update']); 
 
     Route::post('posts/{post}/like', [LikeController::class, 'store']);
     Route::post('posts/{post}/comment', [CommentController::class, 'store']);
