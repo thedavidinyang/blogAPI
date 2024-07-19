ef080e47b52dd66942b6f26c2695dd3b508b9d8b
David Inyang <davidinyang01@gmail.com>
2024-07-19 11:38:38 +0100

    seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('1234567890'), 
+            // 'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'David Inyang',
+            'email' => 'davidinyang01@gmail.com',
+            'password' => bcrypt('1234567890'),
+        ]);
     }
 }
