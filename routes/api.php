3d410afc2de77d36c1de56c128a1a4df7dd28551
David Inyang <davidinyang01@gmail.com>
2024-07-19 11:35:00 +0100

    like and comment route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +20,9 @@ use App\Http\Controllers\PostController;
 
 Route::apiResource('blogs', BlogController::class);
 Route::apiResource('blogs.posts', PostController::class)->shallow();
+
+Route::post('posts/{post}/like', [LikeController::class, 'store']);
+Route::post('posts/{post}/comment', [CommentController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
