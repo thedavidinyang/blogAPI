6d5ce7584abe950695548121bba24ed5574a07cf
David Inyang <davidinyang01@gmail.com>
2024-07-19 11:31:50 +0100

    blog and post routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::apiResource('blogs', BlogController::class);
+Route::apiResource('blogs.posts', PostController::class)->shallow();
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
